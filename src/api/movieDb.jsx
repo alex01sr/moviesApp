bed0e37e7263d1130550f6fa19cdcba28bf06289
alex01sr <89197642+alex01sr@users.noreply.github.com>
2023-04-25 11:48:38 -0500

    Update movieDb.jsx

--- src/api/movieDb.jsx
+++ src/api/movieDb.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 const movieDB = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params: {
-    api_key: 'dc13dff505a162a429c81c4c035df37e',
+    api_key: '',
     language: 'es-ES',
   },
 });
